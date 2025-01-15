6f687ceace9f1c723341aea630f8e101fbcc24b4
J3richoDev <nomat.dev@gmail.com>
2025-01-15 18:22:08 +0800

    done

--- src/main/java/com/chat/realtime/config/WebSocketConfig.java
+++ src/main/java/com/chat/realtime/config/WebSocketConfig.java
@@ -2,26 +2,22 @@ package com.chat.realtime.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
-import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
-import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
-import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurationSupport;
+import org.springframework.web.socket.config.annotation.*;;
 
 @Configuration
 @EnableWebSocketMessageBroker
-public class WebSocketConfig extends WebSocketMessageBrokerConfigurationSupport{
-
+public interface WebSocketConfig extends WebSocketMessageBrokerConfigurer{
+    
     @Override
-    protected void registerStompEndpoints(StompEndpointRegistry registry) {
+    default void registerStompEndpoints(StompEndpointRegistry registry) {
         registry.addEndpoint("/chat")
-            .setAllowedOrigins("http://localhost:5173")
+            .setAllowedOrigins("*")
             .withSockJS();
     }
 
     @Override
-    protected void configureMessageBroker(MessageBrokerRegistry registry) {
-        //set message broker
+    default void configureMessageBroker(MessageBrokerRegistry registry) {
         registry.enableSimpleBroker("/topic");
         registry.setApplicationDestinationPrefixes("/app");
     }
-
 }
--- src/main/java/com/chat/realtime/controller/ChatController.java
+++ src/main/java/com/chat/realtime/controller/ChatController.java
@@ -5,6 +5,8 @@ import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.chat.realtime.models.ChatMessage;
+import org.springframework.web.bind.annotation.GetMapping;
+
 
 @Controller
 public class ChatController {
@@ -14,4 +16,10 @@ public class ChatController {
     public ChatMessage sendMessage(ChatMessage message){
         return message;
     }
+
+    @GetMapping("chat")
+    public String chat() {
+        return "chat";
+    }
+    
 }
